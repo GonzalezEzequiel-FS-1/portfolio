e313d7584ce7237810777993a3b9ba0179c78abb
Ezequiel Gonzalez <ezequiel.gonzalez@egwebdev.com>
2025-09-19 15:21:09 -0400

    Analytics integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import React, { useContext } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { AuthProvider } from "./auth/UseAuth";
-
 import UserLayout from "./RouteLayouts/UserLayout";
 import Home from "./pages/Home";
-import BlogPage from "../src/pages/BlogPage";
+import BlogPage from "./pages/BlogPage";
 import Login from "./pages/Login";
 import AdminPanel from "./AdminPanel/AdminPanel";
 import PrivateRoute from "./PrivateRoutes/PrivateRoute";
@@ -15,50 +19,64 @@ import IndividualPost from "./pages/IndividualPost";
 import { PostProvider } from "../context/PostContext";
 import AboutMe from "./pages/AboutMe";
 import ContactMe from "./pages/ContactMe";
-import NewFeatureCards from "./components/cards/NewFeatureCards";
 import CardSection from "./components/cards/cardSection/CardSection";
+import { analytics } from "../firebaseConfig";
+import { logEvent } from "firebase/analytics";
+
+// Wrapper to track page views
+const RouteTracker = ({ children }) => {
+  const location = useLocation();
+  useEffect(() => {
+    logEvent(analytics, "page_view", { page_path: location.pathname });
+  }, [location]);
+  return children;
+};
 
 const App = () => {
   const { bg } = useContext(ThemeContext);
+
   return (
     <Router>
-      <AuthProvider>
-        <PostProvider>
-          <div
-            className={`${bg} min-h-screen flex flex-col w-screen overflow-x-hidden`}
-          >
-            <Routes>
-              {/* User-facing pages */}
-              <Route element={<UserLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<AboutMe />} />
-                <Route path="/contact" element={<ContactMe />} />
-                <Route path="/test" element={<CardSection />} />
-              </Route>
+      <RouteTracker>
+        <AuthProvider>
+          <PostProvider>
+            <div
+              className={`${bg} min-h-screen flex flex-col w-screen overflow-x-hidden`}
+            >
+              <Routes>
+                {/* User-facing pages */}
+                <Route element={<UserLayout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<AboutMe />} />
+                  <Route path="/contact" element={<ContactMe />} />
+                  <Route path="/test" element={<CardSection />} />
+                </Route>
 
-              {/* Admin pages */}
-              <Route element={<AdminLayout />}>
-                <Route path="/login" element={<Login />} />
-              </Route>
+                {/* Admin pages */}
+                <Route element={<AdminLayout />}>
+                  <Route path="/login" element={<Login />} />
+                </Route>
 
-              {/* Protected admin pages */}
-              <Route element={<PrivateRoute />}>
-                <Route path="/blogpost" element={<AdminPanel />} />
-              </Route>
+                {/* Protected admin pages */}
+                <Route element={<PrivateRoute />}>
+                  <Route path="/blogpost" element={<AdminPanel />} />
+                </Route>
 
-              {/* Blog pages */}
-              <Route element={<PostLayout />}>
-                <Route path="/blogs" element={<BlogPage />} />
-                <Route path="/blogs/:id" element={<IndividualPost />} />
-              </Route>
+                {/* Blog pages */}
+                <Route element={<PostLayout />}>
+                  <Route path="/blogs" element={<BlogPage />} />
+                  <Route path="/blogs/:id" element={<IndividualPost />} />
+                </Route>
 
-              {/* Fallback */}
-              <Route path="*" element={<Home />} />
-            </Routes>
-          </div>
-        </PostProvider>
-      </AuthProvider>
+                {/* Fallback */}
+                <Route path="*" element={<Home />} />
+              </Routes>
+            </div>
+          </PostProvider>
+        </AuthProvider>
+      </RouteTracker>
     </Router>
   );
 };
+
 export default App;
